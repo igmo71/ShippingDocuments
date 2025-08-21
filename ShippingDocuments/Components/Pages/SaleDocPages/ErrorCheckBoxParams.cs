b6d95e7fecceb4dec2ac458ac5d1bc51c669161c
Igor Mogilnitskiy <igmo71@outlook.com>
2025-08-21 17:01:26 +0300

    75660 QuantityErrors component. Remake components.

--- ShippingDocuments/Components/Pages/SaleDocPages/ErrorCheckBoxParams.cs
+++
@@ -1,6 +0,0 @@
-﻿using ShippingDocuments.Domain;
-
-namespace ShippingDocuments.Components.Pages.SaleDocPages
-{
-    public record ErrorCheckBoxParams(PaperworkErrorType ErrorType, bool IsChecked);
-}
